97e1d9050ad070fc82118e796f8c1a917dd0e565
yjs <yujiansong6@163.com>
2019-02-06 10:53:06 +0000

    User can upload avatar

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -11,7 +11,7 @@
                 <div class="panel-body">
                     <div class="media">
                         <div align="center">
-                            <img class="thumbnail img-responsive" src="https://iocaffcdn.phphub.org/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/600/h/600" width="300px" height="300px">
+                            <img class="thumbnail img-responsive" src="{{ $user->avatar }}" width="300px" height="300px">
                         </div>
                         <div class="media-body">
                             <hr>
@@ -19,7 +19,6 @@
                             <p>{{ $user->introduction }}</p>
                             <hr>
                             <h4><strong>注册于</strong></h4>
-{{--                            {{ dd($user->created_at) }}--}}
                             <p>{{ $user->created_at->diffForHumans() }}</p>
                         </div>
                     </div>
