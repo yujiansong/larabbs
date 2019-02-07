6cac7ca7df3da5cf89e649fc6ce4bf33f1765a3c
yjs <yujiansong6@163.com>
2019-02-07 08:30:35 +0000

    Seeding topics and users data

--- database/seeds/TopicsTableSeeder.php
+++ database/seeds/TopicsTableSeeder.php
@@ -7,10 +7,13 @@ class TopicsTableSeeder extends Seeder
 {
     public function run()
     {
-        $topics = factory(Topic::class)->times(50)->make()->each(function ($topic, $index) {
-            if ($index == 0) {
-                // $topic->field = 'value';
-            }
+        $user_ids = \App\Models\User::all()->pluck('id')->toArray();
+        $category_ids = \App\Models\Category::all()->pluck('id')->toArray();
+        $faker = app(Faker\Generator::class);
+        $topics = factory(Topic::class)->times(100)->make()->each(function ($topic, $index) use ($user_ids, $category_ids, $faker)
+        {
+            $topic->user_id = $faker->randomElement($user_ids);
+            $topic->category_id = $faker->randomElement($category_ids);
         });
 
         Topic::insert($topics->toArray());
